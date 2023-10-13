760817bb8aec87f39885abf6362b6aaa688b8b73
Daniel Mauricio B <mauricio.bueno@utp.edu.co>
2023-10-13 14:02:58 -0500

    Se crea el proyecto con las primera dependencias (web,devtool,lombok,mysql,jpa), configuracion de .properties y creacion de la DB en Mysql

---
+++ src/main/java/com/clientes/rest/ClientesSpringRestApplication.java
@@ -0,0 +1,13 @@
+package com.clientes.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientesSpringRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientesSpringRestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/clientes/rest/ClientesSpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clientes.rest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientesSpringRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
